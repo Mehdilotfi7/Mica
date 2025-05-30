1a1d41c45cc48f507f2c28cf0b5f3bfb00860a1e
Mehdilotfi7 <mehdilotfi.tabrizu@gmail.com>
2025-05-30 16:58:00 +0200

    documentation link works now.

--- docs/make.jl
+++ docs/make.jl
@@ -1,22 +1,22 @@
-using TSCPDetector
-using Documenter
+using Documenter, TSCPDetector
 
-#DocMeta.setdocmeta!(TSCPDetector, :DocTestSetup, :(using TSCPDetector); recursive=true)
 
 makedocs(;
-    modules=[TSCPDetector],
-    authors="Mehdi Lotfi",
-    sitename="TSCPDetector.jl",
-    format=Documenter.HTML(),
-    remotes = Dict(),
-    pages = [
-        "Home" => "index.md",
-        "Getting Started" => "getting_started.md",
-        "API" => "api.md",
-    ],
+  modules = [TSCPDetector],
+  doctest = true,
+  linkcheck = false, # Rely on Lint.yml/lychee for the links
+  authors = "Mehdi Lotfi <mehdilotfi.tabrizu@gmail.com>",
+  repo = "https://github.com/mehdilotfi7/TSCPDetector.jl/blob/{commit}{path}#{line}",
+  sitename = "TSCPDetector.jl",
+  format = Documenter.HTML(;
+    prettyurls = false,
+    canonical = "https://mehdilotfi7.github.io/TSCPDetector.jl",
+    assets = ["assets/style.css"],
+  ),
+  pages = [
+    "Home" => "index.md"
+  ],
+  checkdocs=:warn
 )
 
-#deploydocs(;
-#    repo=nothing,
-#    devbranch="main",
-#)
+deploydocs(; repo = "github.com/mehdilotfi7/TSCPDetector.jl", push_preview = false)
--- docs/src/index.md
+++ docs/src/index.md
@@ -1,15 +1,1 @@
-# TSCPDetector.jl
-
-**TSCPDetector** is a Julia package for detecting change points in time series using simulation-based models and evolutionary optimization.
-
-## Features
-
-- Support for ODE, Difference, and Regression models
-- Flexible change point detection framework
-- Optimization with Evolutionary.jl
-
-## Installation
-
-```julia
-#using Pkg
-#Pkg.add(url="https://github.com/Mehdilotfi7/TSCPDetector.jl")
+hi
