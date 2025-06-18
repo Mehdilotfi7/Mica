611b631e9ca1836c5785c96d6d78516f701abb6b
Mehdilotfi7 <mehdilotfi.tabrizu@gmail.com>
2025-06-18 11:26:54 +0200

    Fix export order: call_penalty_fn now exported after definition

--- src/TSCPDetector.jl
+++ src/TSCPDetector.jl
@@ -5,13 +5,7 @@ using DifferentialEquations
 using LabelledArrays
 using Plots
 
-# Export necessary functions
-export AbstractModelSpec, ODEModelSpec, DifferenceModelSpec, RegressionModelSpec, simulate_model, exponential_ode_model,
-example_difference_model, example_regression_model,
-ModelManager, get_initial_condition, segment_model, get_model_type,
-extract_parameters, objective_function, wrapped_obj_function, custom_penalty,
-optimize_with_changepoints, update_bounds!, evaluate_segment, detect_changepoints
-call_penalty_fn, method_argnames, default_penalty, BIC_penalty, relative_length_penalty
+
 # Include the necessary files
 #include("_DataHandler.jl")
 include("_ModelSimulation.jl")
@@ -20,6 +14,15 @@ include("_ObjectiveFunction.jl")
 include("_ChangePointDetection.jl")
 include("penalty.jl")
 #include("_Visualisation.jl")
+
+# Export necessary functions
+export AbstractModelSpec, ODEModelSpec, DifferenceModelSpec, RegressionModelSpec, simulate_model, exponential_ode_model,
+example_difference_model, example_regression_model,
+ModelManager, get_initial_condition, segment_model, get_model_type,
+extract_parameters, objective_function, wrapped_obj_function, custom_penalty,
+optimize_with_changepoints, update_bounds!, evaluate_segment, detect_changepoints
+call_penalty_fn, method_argnames, default_penalty, BIC_penalty, relative_length_penalty
+
 
 
 end # module
